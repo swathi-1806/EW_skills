8f9850a34b05bdda1e770cbc183f99187fc823db
swathi bawge <swathiveda4632@gmail.com>
2026-02-07 17:41:34 +0530

    Update wire.v

--- logic_gates/wire.v
+++ logic_gates/wire.v
@@ -1,1 +1,13 @@
+/*
+Wire
+Write a Verilog module that directly connects an input signal to an output signal. Whatever value appears at the input should immediately appear at the output.
+Requirements
+Module Name: top_module
+Inputs: a
+Outputs: y
+*/
 
+module top_module(input a, output y);
+  // Write your code here
+  assign y=a;
+endmodule
