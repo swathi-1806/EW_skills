b77de750b96deacb628b3c15794d249f0392a925
swathi bawge <swathiveda4632@gmail.com>
2026-02-08 22:27:26 +0530

    Update fanout_wire.v

--- logic_gates/fanout_wire.v
+++ logic_gates/fanout_wire.v
@@ -1,1 +1,19 @@
+/*
+2. Fan-Out Wire
+Connect one input a to three outputs y1, y2, and y3 using wires. All outputs must always match the input.
 
+Requirements:
+
+Module Name: top_module
+Inputs: a (1-bit)
+Outputs: y1, y2, y3 (1-bit each)
+*/
+module top_module(input a, output y1, output y2, output y3);
+    // Declare Inputs and outputs here
+
+    // Module functionality here
+    assign y1 = a;
+    assign y2 = a;
+    assign y3 = a;
+
+endmodule
