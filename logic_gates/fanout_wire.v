09dfc8ea51b20513688183747e1b151b83702532
swathi bawge <swathiveda4632@gmail.com>
2026-02-13 20:59:51 +0530

    Update fanout_wire.v

--- logic_gates/fanout_wire.v
+++ logic_gates/fanout_wire.v
@@ -8,7 +8,9 @@ Module Name: top_module
 Inputs: a (1-bit)
 Outputs: y1, y2, y3 (1-bit each)
 */
-module top_module(input a, output y1, output y2, output y3);
+module top_module(a,y1,y2,y3);
+    input a;
+    output y1,y2,y3; 
     // Declare Inputs and outputs here
 
     // Module functionality here
@@ -17,3 +19,17 @@ module top_module(input a, output y1, output y2, output y3);
     assign y3 = a;
 
 endmodule
+
+module tb;
+    reg a;
+    wire y1,y2,y3;
+        top_module dut(a,y1,y2,y3);
+            initial begin
+                repeat(5)begin
+                    a=$random();
+                    $display("a=%0b y1=%0b y2=%0b y3=%0b",a,y1,y2,y3);
+                end
+            end
+endmodule
+    
+    
