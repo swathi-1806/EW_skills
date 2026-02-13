633f4cad8e5379a12168e2a4095f902988ad5853
swathi bawge <swathiveda4632@gmail.com>
2026-02-13 21:04:59 +0530

    Update multiple_drivers.v

--- logic_gates/multiple_drivers.v
+++ logic_gates/multiple_drivers.v
@@ -8,11 +8,23 @@ Module Name: top_module
 Inputs: a, b (1-bit each)
 Outputs: y (1-bit)
 */
-module top_module (
+module top_module (a,b,sel,y);
     input  a,
     input  b,
     input  sel,
     output y
-);
+
     assign y = sel ? a : b;
 endmodule
+
+module tb;
+    reg a,b,sel;
+    output y;
+    top_module dut(a,b,sel,y);
+        initial begin
+            repeat(5)begin
+                {a,b,sel}=$random;
+            end
+        end
+endmodule
+        
