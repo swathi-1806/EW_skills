ad6fd195cc19bca70e8d1de0082d2fa5b6766565
swathi bawge <swathiveda4632@gmail.com>
2026-02-19 20:31:00 +0530

    Update or_primitive.v

--- primitive/or_primitive.v
+++ primitive/or_primitive.v
@@ -1,1 +1,21 @@
 
+/*OR Primitive
+Implement a 2-input OR using the Verilog gate primitive.
+
+ 
+
+Requirements
+
+Module: or2_prim
+Inputs: a, b
+Output: y
+Must instantiate the built-in and primitive (no assign, no always).
+*/
+module or2_prim (
+    input  a,
+    input  b,
+    output y
+);
+    // TODO: instantiate the built-in OR gate primitive
+    or g1(y,a,b);
+endmodule
