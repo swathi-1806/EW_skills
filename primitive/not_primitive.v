36ab5ac08dbe4c223c6e8d50f183fa54c05a1e79
swathi bawge <swathiveda4632@gmail.com>
2026-02-21 18:08:11 +0530

    Update not_primitive.v

--- primitive/not_primitive.v
+++ primitive/not_primitive.v
@@ -11,10 +11,32 @@ Inputs: a
 Output: y
 Must instantiate the built-in not primitive (no assign, no always).
 */
-module and2_prim (
+module not2_prim (
     input  a,
     output y
 );
     // TODO: instantiate the built-in NOT gate primitive
     not g1(y,a);
 endmodule
+
+module tb;
+ reg a;
+ wire y;
+ not2_prim dut(.a(a),.y(y));
+   initial begin
+    a=1;
+    #1;
+     $display("a=%b y=%b",a,y);
+    a=0;
+    #1;
+     $display("a=%b y=%b",a,y);
+    
+    end
+ 
+endmodule
+
+/*
+OUTPUT
+# a=1 y=0
+# a=0 y=1
+*/
