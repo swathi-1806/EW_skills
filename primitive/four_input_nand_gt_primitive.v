a6b65730a6937bd524708377616bb549dfa06f77
swathi bawge <swathiveda4632@gmail.com>
2026-02-23 22:26:37 +0530

    Update four_input_nand_gt_primitive.v

--- primitive/four_input_nand_gt_primitive.v
+++ primitive/four_input_nand_gt_primitive.v
@@ -23,3 +23,16 @@ module nand4_prim (
     // TODO: instantiate the built-in NAND gate primitive
     nand g1 (y, a, b, c, d);
 endmodule
+
+module tb;
+    reg a,b,c,d;
+    wire y;
+    nand4_prim dut(a,b,c,d,y);
+    initial begin
+        repeat(5)begin
+            {a,b,c,d}=$random;
+            #1;
+            $display("a=%b b=%b  c=%b d=%b y=%b",a,b,c,d,y);
+        end
+    end
+endmodule
